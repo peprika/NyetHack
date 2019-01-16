4284c69842f601be67f71ed9c208374fe9c3a51d
Riku Pepponen <riku.pepponen@gmail.com>
2019-01-16 21:47:01 +0200

    Let's start learning Kotlin!

---
+++ src/Game.kt
@@ -0,0 +1,48 @@
+// Riku Pepponen
+// Thanks to the Big Nerd Farm!
+
+
+fun main(args: Array<String>) {
+
+    // Declarations
+    val name = "Madrigal"
+    var healthPoints = 89
+    var isBlessed = true
+    var isImmortal = false
+
+    // Aura
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
+
+    // Health status
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+
+    // Player status
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
+    val healthStatus = when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches"
+        in 75..89 -> if (isBlessed) {
+            "has some minor wounds, but is healing quite quickly!"
+        } else {
+            "has some minor wounds."
+        }
+        in 15..74 -> "looks pretty hurt"
+        else -> "is in awful condition!"
+    }
+    return healthStatus
+}
+
+private fun printPlayerStatus(auraColor: String, isBlessed: Boolean, name: String, healthStatus: String) {
+    println("(Aura: $auraColor) " + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$name $healthStatus")
+}
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    return auraColor
+}
