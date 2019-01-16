31508b99fb6f6745b3c774c64ec1279d382b4406
Riku Pepponen <riku.pepponen@gmail.com>
2019-01-16 22:02:16 +0200

    Fireball spell - Version 3 (that was too many fireballs! Now default value is 2)

--- src/Game.kt
+++ src/Game.kt
@@ -20,7 +20,7 @@ fun main(args: Array<String>) {
     printPlayerStatus(auraColor, isBlessed, name, healthStatus)
 
     // Cast a fireball!
-    castFireball(5)
+    castFireball()
 }
 
 private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
@@ -49,6 +49,6 @@ private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean
     return auraColor
 }
 
-private fun castFireball(numFireballs: Int) {
+private fun castFireball(numFireballs: Int = 2) {
     println("A glass of Fireball springs into existence. (x$numFireballs)")
 }
