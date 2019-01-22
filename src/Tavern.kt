1d01c42f38e8e1523a7642b5db829e6f81019ae5
Riku Pepponen <riku.pepponen@gmail.com>
2019-01-22 16:19:11 +0200

    Added a simple tavern (for learning about null values)

---
+++ src/Tavern.kt
@@ -0,0 +1,11 @@
+// Riku Pepponen
+// Thanks to the Big Nerd Farm!
+
+
+fun main(args: Array<String>) {
+    println("What would you like to drink?")
+    var beverage = readLine()
+    // beverage = null
+
+    println(beverage)
+}
