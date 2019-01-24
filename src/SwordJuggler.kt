e63d8bb3dd33c95b94eac5041610e90522a0dcfc
Riku Pepponen <riku.pepponen@gmail.com>
2019-01-24 14:17:58 +0200

    1/3 chance to juggle 2 swords, otherwise null swords

--- src/SwordJuggler.kt
+++ src/SwordJuggler.kt
@@ -2,5 +2,11 @@
 // Thanks to the Big Nerd Ranch!
 
 fun main(args: Array<String>) {
+    var swordsJuggling: Int? = null
+    val isJugglingProficient = (1..3).shuffled().last() == 3
+    if (isJugglingProficient) {
+        swordsJuggling = 2
+    }
 
+    println("You juggle $swordsJuggling swords!")
 }
