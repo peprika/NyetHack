ebfa783486a5af2f5d676f4b9468ea28e95505b9
Riku Pepponen <riku.pepponen@gmail.com>
2019-01-24 14:10:48 +0200

    Capitalize drink name, "Buttered ale" if null

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -4,7 +4,13 @@
 
 fun main(args: Array<String>) {
     println("What would you like to drink?")
-    var beverage = readLine()
+    var beverage = readLine()?.let {
+        if (it.isNotBlank()) {
+            it.capitalize()
+        } else {
+            "Buttered Ale"
+        }
+    }
     // beverage = null
 
     println(beverage)
