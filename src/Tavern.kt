5c192b719e54af854b6962833d739c160e29f25d
Riku Pepponen <riku.pepponen@gmail.com>
2019-01-24 14:42:25 +0200

    Splitting the menu data

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -11,4 +11,11 @@ private fun placeOrder(menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
     println("Madrigal speaks with $tavernMaster about their order")
+
+    val data = menuData.split(',')
+    val drinkType = data[0]
+    val drinkName = data[1]
+    val drinkPrice = data[2]
+    val message = "Madrigal buys a $drinkName ($drinkType) for $drinkPrice."
+    println(message)
 }
