d759e6c4b3729c0e4a6d6bfb810d2bdd0c59eebc
Riku Pepponen <riku.pepponen@gmail.com>
2019-01-28 13:53:36 +0200

    Make player name mutable + custom setter with trim()

--- src/Player.kt
+++ src/Player.kt
@@ -2,8 +2,11 @@
 // Thanks to the Big Nerd Ranch!
 
 class Player {
-    val name = "madrigal"
+    var name = "madrigal"
         get() = field.capitalize()
+        set(value) {
+            field = value.trim()
+        }
 
     fun castFireball(numFireballs: Int = 2) =
         println("A glass of Fireball springs into existence. (x$numFireballs)")
