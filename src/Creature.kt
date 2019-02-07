397e3c4a0c30383b56d8c9bbb7d90600f2d98dbf
Riku Pepponen <riku.pepponen@gmail.com>
2019-02-07 21:40:10 +0200

    Default getter for damageRoll

--- src/Creature.kt
+++ src/Creature.kt
@@ -1,8 +1,13 @@
+import java.util.Random
+
 interface Fightable {
     var healthPoints: Int
     val diceCount: Int
     val diceSides: Int
     val damageRoll: Int
+        get() = (0 until diceCount).map {
+            Random().nextInt(diceSides + 1)
+        }.sum()
 
     fun attack(opponent: Fightable): Int
 }
