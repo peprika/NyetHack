3fdee47ad33f1e52884da70bfe7f1b2d9b358a9b
Riku Pepponen <riku.pepponen@gmail.com>
2019-02-07 21:26:02 +0200

    Implementation for dice and attack()

--- src/Player.kt
+++ src/Player.kt
@@ -7,6 +7,20 @@ class Player (_name: String,
               override var healthPoints: Int = 100,
               val isBlessed: Boolean,
               private val isImmortal: Boolean) : Fightable {
+
+    override val diceCount = 3
+    override val diceSides = 6
+    override fun attack(opponent: Fightable): Int {
+        val damageDealt = if (isBlessed) {
+            damageRoll * 2
+        } else {
+            damageRoll
+        }
+        opponent.healthPoints -= damageDealt
+        return damageDealt
+    }
+
+
     // Some declarations
     var name = _name
         get() = "${field.capitalize()} of $hometown"
