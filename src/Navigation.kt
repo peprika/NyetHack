270953ed365c5295f60273ca7157dbe8c4529ba8
Riku Pepponen <riku.pepponen@gmail.com>
2019-02-07 18:33:38 +0200

    Overloading the plus operator

--- src/Navigation.kt
+++ src/Navigation.kt
@@ -6,12 +6,11 @@ enum class Direction(private val coordinate: Coordinate) {
     WEST(Coordinate(-1, 0));
 
     fun updateCoordinate(playerCoordinate: Coordinate) =
-        Coordinate(
-            playerCoordinate.x + coordinate.x,
-            playerCoordinate.y + coordinate.y
-        )
+        coordinate + playerCoordinate
 }
 
 data class Coordinate(val x: Int, val y: Int) {
     val isInBounds = x >= 0 && y >= 0
+
+    operator fun plus(other: Coordinate) = Coordinate(x + other.x, y + other.y)
 }
