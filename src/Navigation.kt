f6d78061a72335a794c5f8c8344de9bfac3373b8
Riku Pepponen <riku.pepponen@gmail.com>
2019-02-07 18:22:26 +0200

    A primary enum constructor

--- src/Navigation.kt
+++ src/Navigation.kt
@@ -1,10 +1,11 @@
 
-enum class Direction {
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST
+enum class Direction(private val coordinate: Coordinate) {
+    NORTH(Coordinate(0, -1)),
+    EAST(Coordinate(1, 0)),
+    SOUTH(Coordinate(0, 1)),
+    WEST(Coordinate(-1, 0))
 }
+
 data class Coordinate(val x: Int, val y: Int) {
     val isInBounds = x >= 0 && y >= 0
 }
