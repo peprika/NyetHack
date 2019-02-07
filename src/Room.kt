11b1b6ef91f58272717f4a5a71cd95c94f77ed83
Riku Pepponen <riku.pepponen@gmail.com>
2019-02-07 17:33:23 +0200

    Town square now has a bell. GWONG!

--- src/Room.kt
+++ src/Room.kt
@@ -9,6 +9,9 @@ open class Room(val name: String) {
 
 class TownSquare : Room("Town Square") {
     override val dangerLevel = super.dangerLevel - 3
+    private var bellSound = "GWONG"
 
-    override fun load() = "The villagers rally and cheer as you enter!"
+    override fun load() = "The villagers rally and cheer as you enter!\n${ringBell()}"
+
+    private fun ringBell() = "The bell tower announces you arrrival, $bellSound!"
 }
